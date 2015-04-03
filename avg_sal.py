2a6c1ba739b2bbac58b59e824434c9af14daa85c
dabarera <ranga@miami.edu>
2015-04-03 15:32:29 -0400

    The first commit

---
+++ avg_sal.py
@@ -0,0 +1,23 @@
+import sys
+import sqlite3
+
+def get_avg_sal(family):
+	connection = sqlite3.connect('survey.db')
+	cursor = connection.cursor()   # abstraction
+
+	sql = "SELECT avg(reading) \
+			FROM Person JOIN Survey \
+			ON Person.ident = Survey.person \
+			WHERE Survey.quant = 'sal' \
+			AND Person.family = ?" 
+
+	cursor.execute(sql, [family])	
+	results = cursor.fetchall()	#  to get tuples
+	sal = results[0][0]
+	connection.close()
+	return sal
+
+for line in sys.stdin:
+	family = line.strip()
+	sal = get_avg_sal(family)
+	print sal, family 
